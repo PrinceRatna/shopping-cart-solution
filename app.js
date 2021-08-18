264b80d790b3d67770c61eea3a947b9780beb8bd
PrinceRatna <arjnry125@gmail.com>
2021-08-19 04:04:59 +0600

    first commit1

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ function add(plusButton,inputValue,phoneAmount,value,isTrue){
         if(isTrue){
             inputValueContainer=parseInt( inputContity.value)+1;
             phoneAmountText.innerText=value*inputValueContainer; 
-
         }
          else if(inputContity.value>0){
              inputValueContainer=parseInt( inputContity.value)-1;
@@ -14,22 +13,17 @@ function add(plusButton,inputValue,phoneAmount,value,isTrue){
          }
          inputContity.value=inputValueContainer; 
          sub(); 
-     })
-    
+     }) 
 }
 function sub(){
     var subTotal=document.getElementById("sub-total");
     var vatValue=document.getElementById("vat-value");
     var totalValue=document.getElementById("total-value");
-
     var blackPhoneAmount=document.getElementById("black-phone-amount");
     var casePhoneAmount=document.getElementById("case-phone-amount");
     subTotal.innerText=parseInt(blackPhoneAmount.innerText)+parseInt(casePhoneAmount.innerText);
     vatValue.innerText=parseInt(subTotal.innerText)/10;
    totalValue.innerText=parseInt(vatValue.innerText)+parseInt(subTotal.innerText);
-
-
-
 }
 add("plus-btn-black","input-value-black","black-phone-amount",1219,true);
 add("plus-btn-case","input-value-case","case-phone-amount",59,true);
